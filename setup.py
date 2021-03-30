8f6f398007ba0138f9a9126c4c86ec0ccf725514
Logan Diamond <Lambda-Logan@users.noreply.github.com>
2021-03-30 12:14:06 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='Address Hammer',
+      version='0.1',
+      description='Tool for processing U.S residential addresses',
+      author='Logan Dimond',
+      author_email='lambda.dimond@gmail.com',
+      url='https://github.com/Lambda-Logan/address_hammer',
+      packages=['address_hammer'],
+     )
